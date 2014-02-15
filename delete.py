c2d6379858bd730bba8ed982525092c23c864eb8
Henri Sweers <pandanomic@gmail.com>
2014-02-15 04:17:38 -0600

    trying 5 seconds now

--- delete.py
+++ delete.py
@@ -7,13 +7,14 @@ import time
 def delete_post(usr, pwd, post_id):
     print "STARTING DELETE"
     post_to_delete = "http://www.facebook.com/" + post_id
+    sleep_time = 5
 
     with contextlib.closing(webdriver.PhantomJS()) as driver:
         # or you can use Firefox()
         # or you can use Chrome(executable_path="/usr/bin/chromedriver")
         print "Opening facebook..."
         driver.get("http://www.facebook.org")
-        time.sleep(2)
+        time.sleep(sleep_time)
         assert "Facebook" in driver.title
 
         print "Logging in..."
@@ -25,12 +26,12 @@ def delete_post(usr, pwd, post_id):
 
         print "Retrieving post..."
         driver.get(post_to_delete)
-        time.sleep(2)
+        time.sleep(sleep_time)
 
         print "Expanding arrow options..."
         elem = driver.find_element_by_xpath("//a[contains(@class,'_5pbj _p')]")
         elem.click()
-        time.sleep(2)
+        time.sleep(sleep_time)
 
         print "Finding delete option..."
         elem = driver.find_element_by_xpath("//div[contains(@class,'_54ng')]")
@@ -38,7 +39,7 @@ def delete_post(usr, pwd, post_id):
 
         print "Clicking delete..."
         links[2].click()
-        time.sleep(2)
+        time.sleep(sleep_time)
 
         print "Finding delete confirmation 'enter'..."
         elem = driver.find_element_by_css_selector("button._42fu:nth-child(1)")
