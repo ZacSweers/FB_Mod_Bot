2a1a30f61db63e1eb9518483df88c79ca3885c3a
Henri Sweers <pandanomic@gmail.com>
2014-02-17 01:36:35 -0600

    Updated deletion code

--- delete.py
+++ delete.py
@@ -7,53 +7,100 @@ import time
 
 def delete_post(usr, pwd, post):
     print "STARTING DELETE"
-    sleep_time = 0
 
-    with contextlib.closing(webdriver.PhantomJS()) as driver:
+    with contextlib.closing(webdriver.Firefox()) as driver:
         # or you can use Firefox()
         # or you can use Chrome(executable_path="/usr/bin/chromedriver")
         print "--Opening facebook..."
         driver.get("http://www.facebook.org")
-        time.sleep(sleep_time)
         assert "Facebook" in driver.title
 
         print "--Logging in..."
-        elem = WebDriverWait(driver, 10).until(lambda driver : driver.find_element_by_id("email"))
-        time.sleep(sleep_time)
+        while True:
+            try:
+                elem = driver.find_element_by_id("email")
+                break
+            except:
+                print "----Waiting"
+                time.sleep(1)
+                timeout_counter += 1
+                if timeout_counter == 5:
+                    raise Exception("TimedOut")
+
+        timeout_counter = 0
         elem.send_keys(usr)
-        time.sleep(sleep_time)
-        elem = WebDriverWait(driver, 10).until(lambda driver : driver.find_element_by_id("pass"))
-        time.sleep(sleep_time)
+
+        while True:
+            try:
+                elem = driver.find_element_by_id("pass")
+                break
+            except:
+                print "----Waiting"
+                time.sleep(1)
+                timeout_counter += 1
+                if timeout_counter == 5:
+                    raise Exception("TimedOut")
+
+        timeout_counter = 0
         elem.send_keys(pwd)
-        time.sleep(sleep_time)
         elem.send_keys(Keys.RETURN)
-        time.sleep(sleep_time)
 
         print "--Retrieving post..."
         driver.get(post)
-        time.sleep(sleep_time)
 
         print "--Expanding arrow options..."
-        elem = WebDriverWait(driver, 10).until(lambda driver : driver.find_element_by_xpath("//a[contains(@class,'_5pbj _p')]"))
-        time.sleep(sleep_time)
-        elem.click()
-        time.sleep(sleep_time)
+        elem = driver.find_elements_by_xpath("//a[contains(@class,'_5pbj _p')]")
+        while len(elem) == 0:
+            print '----Waiting'
+            time.sleep(1)
+            timeout_counter += 1
+            if timeout_counter == 5:
+                raise Exception("TimedOut")
+            elem = driver.find_elements_by_xpath(
+                "//a[contains(@class,'_5pbj _p')]")
+
+        timeout_counter = 0
+        elem[0].click()
 
         print "--Finding delete option..."
-        elem = WebDriverWait(driver, 10).until(lambda driver : driver.find_element_by_xpath("//div[contains(@class,'_54ng')]"))
-        time.sleep(sleep_time)
-        links = WebDriverWait(driver, 10).until(lambda driver : elem.find_elements_by_xpath('.//a'))
-        time.sleep(sleep_time)
+        elem = driver.find_elements_by_xpath("//div[contains(@class,'_54ng')]")
+        while len(elem) == 0:
+            print '----Waiting'
+            time.sleep(1)
+            timeout_counter += 1
+            if timeout_counter == 5:
+                raise Exception("TimedOut")
+            elem = driver.find_elements_by_xpath(
+                "//div[contains(@class,'_54ng')]")
+
+        timeout_counter = 0
+        elem = elem[0]
+        links = elem.find_elements_by_xpath('.//a')
+        while len(links) == 0:
+            print '----Waiting'
+            time.sleep(1)
+            timeout_counter += 1
+            if timeout_counter == 5:
+                raise Exception("TimedOut")
+            links = elem.find_elements_by_xpath('.//a')
 
         print "--Clicking delete..."
+        timeout_counter = 0
         links[2].click()
-        time.sleep(sleep_time)
 
         print "--Finding delete confirmation 'enter'..."
-        elem = WebDriverWait(driver, 10).until(lambda driver : driver.find_element_by_css_selector("button._42fu:nth-child(1)"))
-        time.sleep(sleep_time)
+        elem = driver.find_elements_by_css_selector("button._42fu:nth-child(1)")
+        while len(elem) == 0:
+            print '----Waiting'
+            time.sleep(1)
+            timeout_counter += 1
+            if timeout_counter == 5:
+                raise Exception("TimedOut")
+            elem = driver.find_elements_by_css_selector(
+                "button._42fu:nth-child(1)")
 
+        timeout_counter = 0
         print "--Deleting..."
-        elem.click()
+        elem[0].click()
 
-        print "Deleted"
+        print "--Deleted"
