738f5e59d4f46e470c5c15bba03e16609b482836
Henri Sweers <pandanomic@gmail.com>
2014-03-02 02:51:49 -0600

    Added screenshot uploading on deletion error

--- facebook_sublet_group_bot/delete_post.py
+++ facebook_sublet_group_bot/delete_post.py
@@ -1,12 +1,21 @@
+import pyimgur
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import contextlib
-import time
+import os
+from fb_bot import log, Color, load_properties
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 __author__ = 'Henri Sweers'
+
+
+def imgur_upload(path):
+    client_id = os.environ['imgur_id']
+    im = pyimgur.Imgur(client_id)
+    uploaded_image = im.upload_image(path, title="test?")
+    log("ERROR: " + uploaded_image.link, Color.RED)
 
 
 # Deletes a post using selenium and phantomJS
@@ -21,51 +30,56 @@ def delete_post(usr, pwd, post, *test):
         # or you can use Firefox()
         # or you can use Chrome(executable_path="/usr/bin/chromedriver")
 
-        wait = WebDriverWait(driver, 20)
+        try:
+            wait = WebDriverWait(driver, 20)
 
-        print "--Opening facebook..."
-        driver.get("http://www.facebook.com")
+            print "--Opening facebook..."
+            driver.get("http://www.facebook.com")
 
-        print "--Logging in..."
-        elem = wait.until(ec.presence_of_element_located((By.ID, "email")))
-        elem.send_keys(usr)
-        elem = wait.until(ec.presence_of_element_located((By.ID, "pass")))
-        elem.send_keys(pwd)
-        elem.send_keys(Keys.RETURN)
+            print "--Logging in..."
+            elem = wait.until(ec.presence_of_element_located((By.ID, "email")))
+            elem.send_keys(usr)
+            elem = wait.until(ec.presence_of_element_located((By.ID, "pass")))
+            elem.send_keys(pwd)
+            elem.send_keys(Keys.RETURN)
 
-        print "--Confirming login"
-        elem = wait.until(ec.presence_of_element_located(
-            (By.XPATH, "//a[contains(@class,'fbxWelcomeBoxName')]")))
+            print "--Confirming login"
+            elem = wait.until(ec.presence_of_element_located(
+                (By.XPATH, "//a[contains(@class,'fbxWelcomeBoxName')]")))
 
-        # Assert that login was successful
-        assert(elem.text == "Sublets Bot")
+            # Assert that login was successful
+            assert(elem.text == "Sublets Bot")
 
-        print "--Retrieving post..."
-        driver.get(post)
+            print "--Retrieving post..."
+            driver.get(post)
 
-        print "--Expanding arrow options..."
-        elem = wait.until(ec.presence_of_element_located(
-            (By.XPATH, "//a[contains(@aria-label,'Post options')]")))
-        elem.click()
+            print "--Expanding arrow options..."
+            elem = wait.until(ec.presence_of_element_located(
+                (By.XPATH, "//a[contains(@aria-label,'Post options')]")))
+            elem.click()
 
-        print "--Waiting for options to load..."
-        elem = wait.until(ec.presence_of_element_located(
-            (By.XPATH, "//ul[contains(@class,'_54nf')]")))
+            print "--Waiting for options to load..."
+            elem = wait.until(ec.presence_of_element_located(
+                (By.XPATH, "//ul[contains(@class,'_54nf')]")))
 
-        print "--Finding delete option"
-        links = elem.find_elements_by_xpath(".//a")
+            print "--Finding delete option"
+            links = elem.find_elements_by_xpath(".//a")
 
-        print "--Clicking delete..."
-        if testing:
-            links[3].click()
-        else:
-            links[2].click()
+            print "--Clicking delete..."
+            if testing:
+                links[3].click()
+            else:
+                links[2].click()
 
-        print "--Finding delete confirmation 'enter'..."
-        elem = wait.until(ec.presence_of_element_located(
-            (By.CSS_SELECTOR, "button._42fu:nth-child(1)")))
+            print "--Finding delete confirmation 'enter'..."
+            elem = wait.until(ec.presence_of_element_located(
+                (By.CSS_SELECTOR, "button._42fu:nth-child(1)")))
 
-        print "--Deleting..."
-        elem.click()
+            print "--Deleting..."
+            elem.click()
 
-        print "--Deletion complete"
+            print "--Deletion complete"
+        except Exception as e:
+            driver.save_screenshot('error.png')
+            imgur_upload('error.png')
+            raise e
